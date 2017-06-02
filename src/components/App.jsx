8b01df99805b87eec2f022af4ef955da870467ef
Hack Reactor Student <student@hackreactor.com>
2017-06-02 16:47:45 -0700

    passed App tests

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,29 @@
-var App = () => (
-  <div>
-    <Nav />
-    <div className="col-md-7">
-      <VideoPlayer/>
-    </div>
-    <div className="col-md-5">
-      <VideoList/>
-    </div>
-  </div>
-);
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      value: null
+    };
+
+    // this.videos = {window.exampleVideoData};
+  }
+
+  render() {
+    return (
+      <div>
+        <Nav />
+        <div className="col-md-7">
+         <VideoPlayer/>
+        </div>
+        <div className="col-md-5">
+         <VideoList />
+        </div>
+      </div>
+    );
+  }
+} 
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
